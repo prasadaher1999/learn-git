ffba20001189af3c888c8e6a12478147807ffe36
prasad s aher <prasadaher1999@gmail.com>
2021-07-26 20:15:43 +0530

    add subtraction feature

--- sum.js
+++ sum.js
@@ -1,8 +1,12 @@
 add = (n1,n2)=>{
     return n1+n2;
 }
-
+sub = (n1,n2)=>{
+    return n1-n2;
+}
 let sum = add(1,2);
+let subtraction = sub(1,2);
 
-console.log("welecome ")
-console.log("addition =",sum);
+
+console.log("Addition =",sum);
+console.log("Subtraction =",subtraction);
