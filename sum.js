4afccbf6f9466a3fbdacd06c31aa91c807d710c0
prasad s aher <prasadaher1999@gmail.com>
2021-07-26 23:16:42 +0530

    add division feature

--- sum.js
+++ sum.js
@@ -4,9 +4,14 @@ add = (n1,n2)=>{
 sub = (n1,n2)=>{
     return n1-n2;
 }
+div = (n1,n2)=>{
+    return n1/n2;
+}
+
 let sum = add(1,2);
 let subtraction = sub(1,2);
-
+let divison = div(4,2);
 
 console.log("Addition =",sum);
 console.log("Subtraction =",subtraction);
+console.log("Division =",divison);
